bfaad226de91fdf7ccfc4a5f0285548f75961bcc
Carlos Galeano <dnny419@gmail.com>
2021-05-06 08:07:56 +0700

    First commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if my_list:
+
+        tmp = my_list[0]
+        for i in my_list:
+            if i > tmp:
+                tmp = i
+        return tmp
+    else:
+        return None
