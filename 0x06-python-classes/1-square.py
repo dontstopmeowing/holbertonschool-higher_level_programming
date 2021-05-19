2804f4f88a624113f5ca9cb1ddded36a99c9c4a1
dontstopmeowing <dnny419@gmail.com>
2021-05-19 08:40:23 +0700

    First commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Defining a square.
+    Attributes:
+        __size: size of the square.
+    """
+
+    def __init__(self, size):
+        """Init the Square with a size"""
+        self.__size = size
