e5102ef61b3d9d22d3dbe978c505a254f501800c
dontstopmeowing <dnny419@gmail.com>
2021-06-02 01:20:44 +0700

    Adding initial files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This file contains the following functions:
+"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+    return dir(obj)
