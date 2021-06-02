87a55fb8d1257a460d4e4f0c739063730224b102
dontstopmeowing <dnny419@gmail.com>
2021-06-03 03:52:44 +0700

    Task 5 first commit

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        Writes an Object to a text file, using a JSON representation
+    """
+
+    with open(filename, 'w', encoding='utf-8') as staroba:
+        json.dump(my_obj, staroba)
