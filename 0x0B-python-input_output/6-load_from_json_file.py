40372810d8d8e7c32e171ec09ba5d5bd77ecd390
dontstopmeowing <dnny419@gmail.com>
2021-06-03 05:11:18 +0700

    Task 6 first commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This file contains the function write_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+        Creates an Object from a “JSON file”
+    """
+
+    with open(filename, 'r', encoding='utf-8') as staroba:
+        json.load(staroba)
