579d853165e3b500f80a5536c8be702d0d7b564c
dontstopmeowing <dnny419@gmail.com>
2021-06-03 03:26:19 +0700

    Adding task 3 and fixing task 1 and task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,8 @@
 
 def append_write(filename="", text=""):
     """
-        Creates, write, append text on a file and returns the number of chars written to the file.
+        Creates, write, append text on a file
+        and returns the number of chars written to the file.
     """
 
     with open(filename, 'a+', encoding='utf-8') as staroba:
