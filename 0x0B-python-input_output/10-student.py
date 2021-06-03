760690e0a11338bd4e02a2bebbf060fa21c460b2
dontstopmeowing <dnny419@gmail.com>
2021-06-03 08:27:34 +0700

    Task 10 first commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    This file contains the class Student
+"""
+
+
+class Student:
+    """Class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        new = {}
+        if attrs is None:
+            return(self.__dict__)
+        for i in attrs:
+            try:
+                new[i] = self.__dict__[i]
+            except:
+                pass
+        return new
