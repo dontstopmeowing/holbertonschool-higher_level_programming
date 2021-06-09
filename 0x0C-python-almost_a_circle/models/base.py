309037d1c4cba4654cd48473a1cd623a1ad71864
dontstopmeowing <dnny419@gmail.com>
2021-06-09 08:40:59 +0700

    First commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,13 @@ class Base:
 
         with open(filename, 'w', encoding="utf-8") as file:
             file.write(cls.to_json_string(content))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+            Returns the list of the JSON string representation json_string.
+        """
+        if json_string is None or len(json_string) == 0:
+            return "[]"
+
+        return json.loads(json_string)
