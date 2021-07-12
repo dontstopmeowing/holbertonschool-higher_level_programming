2091aa31d5db2c98bd0caa91bd50ef20a6b99607
dontstopmeowing <dnny419@gmail.com>
2021-07-13 03:14:12 +0700

    Fix

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
 -- Script that updates the score of Bob to 10 in the table second_table from a specific database
 -- You are not allowed to use Bob’s id value, only the name field.
 
-UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob' LIMIT 1;
+UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
 
