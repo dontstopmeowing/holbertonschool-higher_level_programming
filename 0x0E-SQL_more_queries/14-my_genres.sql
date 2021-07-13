281df67faf13034ece9c23d7493709790ae2d663
dontstopmeowing <dnny419@gmail.com>
2021-07-14 05:59:06 +0700

    Final fix for task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,10 +4,10 @@ SELECT
 	`tv_genres`.`name`
 FROM
 	`tv_genres`
-	INNER JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
-	INNER JOIN `tv_shows` ON `tv_show_genres`.`show_id` = `tv_shows`.`id` 
+	RIGHT JOIN `tv_show_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
+	RIGHT JOIN `tv_shows` ON `tv_show_genres`.`show_id` = `tv_shows`.`id` 
 WHERE
-	`tv_shows`.`id` = 8 
+	`tv_shows`.`title` = 'Dexter' 
 GROUP BY
 	`tv_show_genres`.`genre_id` 
 ORDER BY
