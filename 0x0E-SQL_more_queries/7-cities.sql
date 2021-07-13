715d04eb182a66e49ce223ff28fb3158e6788fea
dontstopmeowing <dnny419@gmail.com>
2021-07-14 00:27:46 +0700

    Fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,9 +6,9 @@ IF
 	NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE
 IF
-	NOT EXISTS `cities` (
+	NOT EXISTS `hbtn_0d_usa`.`cities` (
 		`id` INT NOT NULL UNIQUE AUTO_INCREMENT,
 		`state_id` INT NOT NULL,
 		`name` VARCHAR ( 256 ) NOT NULL,
 	PRIMARY KEY ( `id` ),
-	FOREIGN KEY ( `state_id` ) REFERENCES states ( `id` ));
+	FOREIGN KEY ( `state_id` ) REFERENCES `hbtn_0d_usa`.`states` ( `id` ));
