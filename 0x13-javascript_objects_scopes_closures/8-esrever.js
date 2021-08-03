98ace389cc85034ab14ea3ddc14f3676b0396d7e
dontstopmeowing <dnny419@gmail.com>
2021-08-04 04:15:32 +0700

    Fix?

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const tmpList = [];
-  let i = 0;
-  do {
-    tmpList.unshift(list[i]);
-    i++;
-  } while (i < list.length);
+  for (const i of list) {
+    tmpList.unshift(i);
+  }
   return tmpList;
 };
