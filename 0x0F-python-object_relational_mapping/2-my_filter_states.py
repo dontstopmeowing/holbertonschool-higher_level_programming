23f912c63e9452609603c753e40fb6e7d675074d
dontstopmeowing <dnny419@gmail.com>
2021-08-06 20:52:45 +0700

    Fixing comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+Script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 
 import MySQLdb
