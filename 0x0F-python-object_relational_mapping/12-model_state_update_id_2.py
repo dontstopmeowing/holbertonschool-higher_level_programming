2979efcee6efc3a4e1b9c60c83bb796e1582e881
dontstopmeowing <dnny419@gmail.com>
2021-08-07 19:05:16 +0700

    Adding task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that changes the name of a State object from the database hbtn_0e_6_usa.
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        result = session.query(State).filter_by(id = '2').first()
+        result.name = 'New Mexico'
+        session.commit()
+    except Exception:
+        print("Encountered a general SQLAlchemyError. Call 911 immediately!")
+    finally:
+        session.close()
