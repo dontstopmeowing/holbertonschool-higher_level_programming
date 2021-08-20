05380e4e4b2fffea15fef0427b83569996a3ed5e
dontstopmeowing <dnny419@gmail.com>
2021-08-21 03:11:02 +0700

    Adding task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that fetches https://intranet.hbtn.io/status."""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    print("Body response:")
+    print("\t- type: {}".format(type(requests.get(url).text)))
+    print("\t- content: {}".format(requests.get(url).text))
