141d461b25b4550c6e1f01671be087a19f8f0abf
dontstopmeowing <dnny419@gmail.com>
2021-09-15 01:09:58 +0700

    Fix semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*Script that writes a string to a file.*/
+/* Script that writes a string to a file. */
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
